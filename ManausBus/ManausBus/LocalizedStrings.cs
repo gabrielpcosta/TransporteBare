ad6764beb78b8d0b8d97fca19d855fc8d1686b71
GabrielPCosta <gabrielp.costa@outlook.com>
2015-01-23 19:47:48 -0200

    Início do Projeto

---
+++ ManausBus/ManausBus/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using ManausBus.Resources;
+
+namespace ManausBus
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
