b7ed9a9a956b146f3defb6b15f5804b1e2687a26
Gabriel Pereira da Costa <gabrielp.costa@outlook.com>
2015-01-28 23:21:59 -0200

    Atualizado o projeto para Universal Apps Project

---
+++ ManausBus/ManausBus/ManausBus.WindowsPhone/SearchDialogueBox.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ManausBus
+{
+    public sealed partial class SearchDialogueBox : UserControl
+    {
+        public SearchDialogueBox()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
